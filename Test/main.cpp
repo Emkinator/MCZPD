5add1456fffdd093baef393bc016696b2566b8c1
Emk <Emkinator@inbox.lv>
2012-11-25 21:47:46 +0200

    Fixed karlis' error

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,10 +1,8 @@
 #include <windows.h>
 
-int WINAPI;
-WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow)
 {
-    MessageBox(NULL, "Never gonna give you up!",
-        "Never gonna let you down!", MB_OK);
+    MessageBox(NULL, "Never gonna give you up!","Never gonna let you down!", MB_OK);
 
     return 0;
 }
