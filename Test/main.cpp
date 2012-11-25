c3443d40906662dec2ebf4bc1f36edeac2c474aa
Emk <Emkinator@inbox.lv>
2012-11-25 19:21:15 +0200

    Testing

---
+++ Test/main.cpp
@@ -0,0 +1,11 @@
+#include <windows.h>
+
+int WINAPI
+WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow)
+{
+MessageBox(NULL, "Never gonna give you up!",
+"Hello World!", MB_OK);
+
+  return 0;
+}
+
